0f39b44d114135c33398fca7ce7217e23ee0219e
Joshua Mervine <jmervine@users.noreply.github.com>
2024-04-09 19:35:52 -0700

    init prototype commit

---
+++ Makefile
@@ -0,0 +1,23 @@
+RUN := cargo run
+BIN := noop-client
+VERBOSE ?= false
+ARGS ?= --url http://localhost:3000/cli --headers "X-Test-1=makefile1" --headers "X-Test-2=makefile2" -n 5 --input test_requests.txt --verbose=$(VERBOSE)
+
+default: test run_help run run_args
+
+run_help:
+	# help
+	$(RUN) --bin $(BIN) -- --help
+
+run:
+	# no args
+	$(RUN) --bin $(BIN)
+
+run_args:
+	# args
+	$(RUN) --bin $(BIN) -- $(ARGS) 
+
+test:
+	cargo test
+
+.PHONY: test run run_help run_no_args default
---
+++ main.rs
@@ -0,0 +1,258 @@
+extern crate clap;
+extern crate reqwest;
+
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::{error::Error, str::FromStr};
+use clap::Parser;
+use reqwest::blocking::*;
+use reqwest::header::*;
+use reqwest::{Url, Method};
+
+static mut VERBOSE: bool = false;
+
+// -- macros
+macro_rules! is_verbose {
+    () => { ( unsafe { VERBOSE } ) }
+}
+
+macro_rules! set_verbose {
+    ($v:expr) => { 
+        unsafe {
+            match $v {
+                Some(true) =>  { VERBOSE = true },
+                _ => { VERBOSE = false },
+                // Some(false) => { VERBOSE = false },
+                // None => { VERBOSE = false }
+            }
+        }
+    }
+}
+
+// macro_rules! todo {
+//     ($s:expr) => {
+//         println!("[TODO]: {} on line {}", $s, line!())
+//     }
+// }
+
+macro_rules! debug {
+    ($s:expr) => { 
+        if is_verbose!() { 
+            println!("[DEBUG]: {}", $s)
+        }
+    };
+}
+
+macro_rules! unless_debug {
+    ($s:expr) => { 
+        if !is_verbose!() { 
+            print!("{}", $s)
+        }
+    };
+}
+
+
+// ------
+#[derive(Parser,Debug,Clone)]
+struct Config {
+    #[arg(long, short, default_value = "http://localhost:3000/")]
+    url: String,
+
+    #[arg(long,short, default_value = "GET")]
+    method: String,
+
+    #[arg(long, short = 'x', default_value = "")]
+    headers: Vec<String>,
+
+    #[arg(long, short, required = false, default_value = "")]
+    input: String,
+
+    #[arg(long, short, required = false, default_value = "")]
+    ouptut: String,
+
+    // TODO: Make '--verbose' without a value work.
+    #[arg(long, short, default_value = "false")]
+    verbose: Option<bool>,
+
+    #[arg(long, short = 'n', default_value = "1")]
+    iterations: i32,
+}
+
+fn headers(headers: Vec<String>) -> Result<HeaderMap, Box<dyn std::error::Error>> {
+    let mut map = HeaderMap::new(); 
+
+    if headers.len() == 0 || headers[0] == "" {
+        return Ok(map)
+    }
+
+    let mut iter = IntoIterator::into_iter(headers);
+    while let Some(head) = iter.next() {
+        let Some((sname, svalue)) = head.split_once("=") else {
+            return Err(format!("Split error on header string: '{}'", head).into());
+        };
+        let hname = HeaderName::try_from(sname)?;
+        let hvalue = HeaderValue::try_from(svalue)?;
+
+        map.insert(hname, hvalue);
+    }
+
+    Ok(map)
+}
+
+//fn request(config: Config) -> Result<String, Box<dyn Error>> {
+fn request(method: &str, endpoint: &str, headers: HeaderMap) -> Result<String, Box<dyn Error>> {
+    let client = Client::builder()
+        .default_headers(headers)
+        .build()?;
+
+    let url = Url::parse(endpoint)?;
+    let meth = Method::from_str(method)?;
+    let req = Request::new(meth, url);
+
+    let resp = client.execute(req)?; 
+    let body = resp.text()?;
+
+    debug!("---");
+    debug!(body);
+    unless_debug!("+");
+    Ok(body)
+}
+
+fn cli_request(cfg: Config) -> Result<String, Box<dyn Error>> {
+    let headers = headers(cfg.headers)?;
+    let res = request(
+        &cfg.method, &cfg.url, headers
+    );
+
+    let body = match res {
+        Ok(s) => s,
+        Err(e) => format!("ERROR: {}", e.to_string()), 
+    };
+
+    Ok(body)
+}
+
+fn do_requests(cfg: Config) -> Result<Vec<String>, Box<dyn Error>> {
+    debug!(format!("{:#?}", cfg));
+
+    let iters: i32 = if cfg.iterations == 0 { 1 } else { cfg.iterations };
+
+    let mut output: Vec<String> = Vec::new();
+
+    if cfg.input.len() == 0 {
+        for _ in 0..iters {
+            let res = cli_request(cfg.clone())?;
+            output.push(res);
+        }
+        return Ok(output)
+    }
+
+    let input = File::open(&cfg.input)?;
+    let reader = BufReader::new(input);
+
+    for line in reader.lines() {
+        match line {
+            Ok(l) => {
+                debug!(format!("Extracting: '{}'", l));
+
+                let mut parts = l.split('|');
+                let (mut method, mut url, sheaders) = (
+                    parts.next().unwrap_or(""),
+                    parts.next().unwrap_or(""),
+                    parts.next().unwrap_or("")
+                );
+
+                if method.len() == 0 || method == "" {
+                    method = &cfg.method;
+                }
+
+                if url.len() == 0 || url == "" {
+                    url = &cfg.url;
+                }
+
+                let vheaders: Vec<String> = if sheaders.len() == 0 || sheaders == "" {
+                    cfg.headers.clone()
+                } else {
+                    sheaders.split(',')
+                        .map(|s| s.to_string())
+                        .collect()
+                };
+
+                debug!(format!("Headers: '{}'", vheaders.join(",")));
+
+                let headers = headers(vheaders)?;
+                for _ in 0..iters {
+                    let body = request(method, url, headers.clone())?;
+                    output.push(body);
+                }
+            },
+            Err(err) => {
+                eprintln!("Error reading line: {}", err);
+            }
+        }
+    }
+
+    Ok(output)
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let args: Config = Config::parse();
+    set_verbose!(args.verbose);
+
+    let res = do_requests(args)?;
+    unless_debug!(format!("\nRequests made: {}", res.len()));
+    unless_debug!("\n");
+
+    Ok(())
+}
+
+// ------
+#[cfg(test)]
+mod tests {
+    use reqwest::header::CONTENT_TYPE;
+
+    use super::*;
+
+    #[test]
+    fn headers_ok_test() {
+        // reused
+        let hval1 = HeaderValue::from_str("application/json").unwrap();
+        let hval2 = HeaderValue::from_str("testing").unwrap();
+
+        // ok
+        let hvec = vec![ 
+            "Content-Type=application/json".to_string(), // standard
+            "X-Foobar=testing".to_string()               // custom
+        ];
+        let mut expected = HeaderMap::new();
+        expected.insert(CONTENT_TYPE, hval1);
+        expected.insert("X-Foobar", hval2);
+
+        let result  = headers(hvec).unwrap();
+        assert_eq!(expected, result);
+    }
+
+    #[test]
+    fn headers_bad_test() {
+        let badvec = vec![ 
+            "Content-Type::application/json".to_string() // bad split
+        ];
+        assert!(headers(badvec).is_err(), "should error when not able to split on '='"); 
+    }
+
+    #[test]
+    fn headers_empty_name_test() {
+        let badvec = vec![ 
+            "=application/json".to_string() // empty name 
+        ];
+        assert!(headers(badvec).is_err(), "shouldn't allow empty name"); 
+    }
+
+    #[test]
+    fn headers_empty_value_test() {
+        let badvec = vec![ 
+            "content-type=".to_string() // empty value 
+        ];
+        assert!(!headers(badvec).is_err(), "should allow empty value"); 
+    }
+}
